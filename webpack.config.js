521a4dbcbcab0ef317bc8abe74de31baf11c7822
Nikita Konkin <niki-konkin@yandex.ru>
2021-10-15 15:13:33 +0300

    bugfix

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: {
-		main: './pages/index.js'
+		main: './src/pages/index.js'
 	},
 	output: {
 		path: path.resolve(__dirname,'dist/'),
@@ -53,7 +53,7 @@ module.exports = {
   },
   plugins: [
 	  new HtmlWebpackPlugin({
-	    template: './index.html' // путь к файлу index.html
+	    template: './src/index.html' // путь к файлу index.html
 	  }),
 	  new CleanWebpackPlugin(),
 	  new MiniCssExtractPlugin()
