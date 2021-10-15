21e346f894af2573de90b7f4564ef8fc549177f8
Nikita Konkin <niki-konkin@yandex.ru>
2021-10-15 21:16:56 +0300

    bugfix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
 import './index.css';
 
+const profileName = document.querySelector('.profile__name');
+const profileCareer = document.querySelector('.profile__career');
+
+const formProfile = document.querySelector('.form_type_profile');
+const formProfileName = document.querySelector('.form__decription-input_type_profile-name');
+const formProfileCareer = document.querySelector('.form__decription-input_type_profile-career');
+
+const formPhoto = document.querySelector('.form_type_add-photo');
+const formPhotoName = document.querySelector('.form__decription-input_type_photo-name');
+const formPhotoLink = document.querySelector('.form__decription-input_type_photo-link');
+
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupProfileCloseButton = document.querySelector('.popup__close-button_type_profile');
+
+const popupAddPhoto = document.querySelector('.popup_type_add-photo');
+const popupAddPhotoCloseButton = document.querySelector('.popup__close-button_type_add-photo');
+
+const popupPhoto = document.querySelector('.popup-photo');
+const popupPhotoCloseButton = document.querySelector('.popup-photo__close-button');
+
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileAddButton = document.querySelector('.profile__add-button');
+
+const template = document.querySelector('#element').content;
+
 import {Card} from '../components/card.js';
 import {initialCards} from '../utils/cards.js';
 import {FormValidator} from '../components/formValidator.js'
@@ -9,11 +34,7 @@ import {PopupWithImage} from '../components/PopupWithImage.js'
 import {PopupWithForm} from '../components/PopupWithForm.js'
 import {UserInfo} from '../components/UserInfo.js'
 
-import {profileName, profileCareer, formProfile, formProfileName, formProfileCareer,
-				formPhoto, formPhotoName, formPhotoLink, popupProfile, popupProfileCloseButton,
-				popupAddPhoto, popupAddPhotoCloseButton, popupPhoto, popupPhotoCloseButton,
-				profileEditButton, profileAddButton, template,
-				container, popupPhotoImageElement, popupPhotoTitleElement} from '../utils/constants.js'
+import {container, popupPhotoImageElement, popupPhotoTitleElement} from '../utils/constants.js'
 
 const editProfileValidator = new FormValidator(validationClasses, formProfile);
 editProfileValidator.enableValidation();
@@ -57,7 +78,7 @@ const popupFormChangeProfile = new PopupWithForm(
 	() => {addCardValidator.toggleButtonState()})
 popupFormChangeProfile.setEventListeners();
 
-function openPopupAddNewCard(){
+function openPopupEditProfile(){
 
   popupFormChangeProfile.open();
 
@@ -66,12 +87,12 @@ function openPopupAddNewCard(){
 
 }
 
-function openPopupAddProfilePhoto(){
+function openPopupAddNewCard(){
 
   popupFormAddPhoto.open();
   
 }
 
-profileEditButton.addEventListener('click',  openPopupAddNewCard);
-profileAddButton.addEventListener('click',  openPopupAddProfilePhoto);
+profileEditButton.addEventListener('click', openPopupEditProfile);
+profileAddButton.addEventListener('click', openPopupAddNewCard);
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,34 +1,6 @@
-const profileName = document.querySelector('.profile__name');
-const profileCareer = document.querySelector('.profile__career');
-
-const formProfile = document.querySelector('.form_type_profile');
-const formProfileName = document.querySelector('.form__decription-input_type_profile-name');
-const formProfileCareer = document.querySelector('.form__decription-input_type_profile-career');
-
-const formPhoto = document.querySelector('.form_type_add-photo');
-const formPhotoName = document.querySelector('.form__decription-input_type_photo-name');
-const formPhotoLink = document.querySelector('.form__decription-input_type_photo-link');
-
-const popupProfile = document.querySelector('.popup_type_profile');
-const popupProfileCloseButton = document.querySelector('.popup__close-button_type_profile');
-
-const popupAddPhoto = document.querySelector('.popup_type_add-photo');
-const popupAddPhotoCloseButton = document.querySelector('.popup__close-button_type_add-photo');
-
-const popupPhoto = document.querySelector('.popup-photo');
-const popupPhotoCloseButton = document.querySelector('.popup-photo__close-button');
-
-const profileEditButton = document.querySelector('.profile__edit-button');
-const profileAddButton = document.querySelector('.profile__add-button');
-
-const template = document.querySelector('#element').content;
 const container = '.elements';
 
 const popupPhotoImageElement = '.popup-photo__img';
 const popupPhotoTitleElement = '.popup-photo__photo-descriprion';
 
-export {profileName, profileCareer, formProfile, formProfileName, formProfileCareer,
-				formPhoto, formPhotoName, formPhotoLink, popupProfile, popupProfileCloseButton,
-				popupAddPhoto, popupAddPhotoCloseButton, popupPhoto, popupPhotoCloseButton,
-				profileEditButton, profileAddButton, template,
-				container, popupPhotoImageElement, popupPhotoTitleElement}
+export {container, popupPhotoImageElement, popupPhotoTitleElement}
